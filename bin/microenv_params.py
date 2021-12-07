91e471a83cc1130be6ea4c2a6cde96c52c8a56a8
Randy Heiland <heiland@indiana.edu>
2021-12-07 10:34:37 -0500

    add distance fields

--- bin/microenv_params.py
+++ bin/microenv_params.py
@@ -64,6 +64,46 @@ class MicroenvTab(object):
         self.oxygen_Dirichlet_boundary_condition_toggle_zmax = Checkbox(description='zmax', disabled=False,style=style, layout=widget_layout)
 
         self.oxygen_Dirichlet_boundary_condition_value_zmax = FloatText(value=10,style=style, layout=widget2_layout)
+
+        menv_var2 = Button(description='glom_cap_distance', disabled=True, layout=name_button_layout)
+        menv_var2.style.button_color = 'lightgreen'
+
+        param_name5 = Button(description='diffusion_coefficient', disabled=True, layout=name_button_layout)
+
+        self.glom_cap_distance_diffusion_coefficient = FloatText(value=0.0,
+          step=0.01,style=style, layout=widget_layout)
+
+        param_name6 = Button(description='decay_rate', disabled=True, layout=name_button_layout)
+
+        self.glom_cap_distance_decay_rate = FloatText(value=0,
+          step=0.01,style=style, layout=widget_layout)
+        param_name7 = Button(description='initial_condition', disabled=True, layout=name_button_layout)
+
+        self.glom_cap_distance_initial_condition = FloatText(value=0,style=style, layout=widget_layout)
+        param_name8 = Button(description='Dirichlet_boundary_condition', disabled=True, layout=name_button_layout)
+
+        self.glom_cap_distance_Dirichlet_boundary_condition = FloatText(value=0,style=style, layout=widget_layout)
+        self.glom_cap_distance_Dirichlet_boundary_condition_toggle = Checkbox(description='on/off', disabled=False,style=style, layout=widget_layout)
+
+        menv_var3 = Button(description='glom_bm_distance', disabled=True, layout=name_button_layout)
+        menv_var3.style.button_color = 'tan'
+
+        param_name9 = Button(description='diffusion_coefficient', disabled=True, layout=name_button_layout)
+
+        self.glom_bm_distance_diffusion_coefficient = FloatText(value=0.0,
+          step=0.01,style=style, layout=widget_layout)
+
+        param_name10 = Button(description='decay_rate', disabled=True, layout=name_button_layout)
+
+        self.glom_bm_distance_decay_rate = FloatText(value=0,
+          step=0.01,style=style, layout=widget_layout)
+        param_name11 = Button(description='initial_condition', disabled=True, layout=name_button_layout)
+
+        self.glom_bm_distance_initial_condition = FloatText(value=0,style=style, layout=widget_layout)
+        param_name12 = Button(description='Dirichlet_boundary_condition', disabled=True, layout=name_button_layout)
+
+        self.glom_bm_distance_Dirichlet_boundary_condition = FloatText(value=0,style=style, layout=widget_layout)
+        self.glom_bm_distance_Dirichlet_boundary_condition_toggle = Checkbox(description='on/off', disabled=False,style=style, layout=widget_layout)
         self.calculate_gradient = Checkbox(description='calculate_gradients', disabled=False, layout=desc_button_layout)
         self.track_internal = Checkbox(description='track_in_agents', disabled=False, layout=desc_button_layout)
 
@@ -73,6 +113,14 @@ class MicroenvTab(object):
         menv_units_button2 = Button(description='1/min', disabled=True, layout=units_button_layout) 
         menv_units_button3 = Button(description='mmHg', disabled=True, layout=units_button_layout) 
         menv_units_button4 = Button(description='mmHg', disabled=True, layout=units_button_layout) 
+        menv_units_button5 = Button(description='micron^2/min', disabled=True, layout=units_button_layout) 
+        menv_units_button6 = Button(description='1/min', disabled=True, layout=units_button_layout) 
+        menv_units_button7 = Button(description='mmHg', disabled=True, layout=units_button_layout) 
+        menv_units_button8 = Button(description='mmHg', disabled=True, layout=units_button_layout) 
+        menv_units_button9 = Button(description='micron^2/min', disabled=True, layout=units_button_layout) 
+        menv_units_button10 = Button(description='1/min', disabled=True, layout=units_button_layout) 
+        menv_units_button11 = Button(description='mmHg', disabled=True, layout=units_button_layout) 
+        menv_units_button12 = Button(description='mmHg', disabled=True, layout=units_button_layout) 
 
 
 
@@ -88,8 +136,18 @@ class MicroenvTab(object):
         row8 = [self.oxygen_Dirichlet_boundary_condition_toggle_xmax, self.oxygen_Dirichlet_boundary_condition_value_xmax,]
         row9 = [self.oxygen_Dirichlet_boundary_condition_toggle_zmin, self.oxygen_Dirichlet_boundary_condition_value_zmin,]
         row10 = [self.oxygen_Dirichlet_boundary_condition_toggle_zmax, self.oxygen_Dirichlet_boundary_condition_value_zmax,]
-        row11 = [self.calculate_gradient,]
-        row12 = [self.track_internal,]
+        row_glom_cap_distance = [menv_var2,  ] 
+        row11 = [param_name5, self.glom_cap_distance_diffusion_coefficient, menv_units_button5]
+        row12 = [param_name6, self.glom_cap_distance_decay_rate, menv_units_button6]
+        row13 = [param_name7, self.glom_cap_distance_initial_condition, menv_units_button7]
+        row14 = [param_name8, self.glom_cap_distance_Dirichlet_boundary_condition, menv_units_button8, self.glom_cap_distance_Dirichlet_boundary_condition_toggle]
+        row_glom_bm_distance = [menv_var3,  ] 
+        row15 = [param_name9, self.glom_bm_distance_diffusion_coefficient, menv_units_button9]
+        row16 = [param_name10, self.glom_bm_distance_decay_rate, menv_units_button10]
+        row17 = [param_name11, self.glom_bm_distance_initial_condition, menv_units_button11]
+        row18 = [param_name12, self.glom_bm_distance_Dirichlet_boundary_condition, menv_units_button12, self.glom_bm_distance_Dirichlet_boundary_condition_toggle]
+        row19 = [self.calculate_gradient,]
+        row20 = [self.track_internal,]
 
 
         box_layout = Layout(display='flex', flex_flow='row', align_items='stretch', width='100%')
@@ -104,8 +162,18 @@ class MicroenvTab(object):
         box8 = Box(children=row8, layout=box_layout)
         box9 = Box(children=row9, layout=box_layout)
         box10 = Box(children=row10, layout=box_layout)
+        box_glom_cap_distance = Box(children=row_glom_cap_distance, layout=box_layout)
         box11 = Box(children=row11, layout=box_layout)
         box12 = Box(children=row12, layout=box_layout)
+        box13 = Box(children=row13, layout=box_layout)
+        box14 = Box(children=row14, layout=box_layout)
+        box_glom_bm_distance = Box(children=row_glom_bm_distance, layout=box_layout)
+        box15 = Box(children=row15, layout=box_layout)
+        box16 = Box(children=row16, layout=box_layout)
+        box17 = Box(children=row17, layout=box_layout)
+        box18 = Box(children=row18, layout=box_layout)
+        box19 = Box(children=row19, layout=box_layout)
+        box20 = Box(children=row20, layout=box_layout)
 
         self.tab = VBox([
           box_oxygen,
@@ -119,8 +187,18 @@ class MicroenvTab(object):
           box8,
           box9,
           box10,
+          box_glom_cap_distance,
           box11,
           box12,
+          box13,
+          box14,
+          box_glom_bm_distance,
+          box15,
+          box16,
+          box17,
+          box18,
+          box19,
+          box20,
         ])
 
     # Populate the GUI widgets with values from the XML
@@ -172,6 +250,24 @@ class MicroenvTab(object):
           self.oxygen_Dirichlet_boundary_condition_toggle_zmax.value = False
         self.oxygen_Dirichlet_boundary_condition_value_zmax.value = float(vp[0].find('.//Dirichlet_options//boundary_value[6]').text)
 
+        self.glom_cap_distance_diffusion_coefficient.value = float(vp[1].find('.//diffusion_coefficient').text)
+        self.glom_cap_distance_decay_rate.value = float(vp[1].find('.//decay_rate').text)
+        self.glom_cap_distance_initial_condition.value = float(vp[1].find('.//initial_condition').text)
+        self.glom_cap_distance_Dirichlet_boundary_condition.value = float(vp[1].find('.//Dirichlet_boundary_condition').text)
+        if vp[1].find('.//Dirichlet_boundary_condition').attrib['enabled'].lower() == 'true':
+          self.glom_cap_distance_Dirichlet_boundary_condition_toggle.value = True
+        else:
+          self.glom_cap_distance_Dirichlet_boundary_condition_toggle.value = False
+
+        self.glom_bm_distance_diffusion_coefficient.value = float(vp[2].find('.//diffusion_coefficient').text)
+        self.glom_bm_distance_decay_rate.value = float(vp[2].find('.//decay_rate').text)
+        self.glom_bm_distance_initial_condition.value = float(vp[2].find('.//initial_condition').text)
+        self.glom_bm_distance_Dirichlet_boundary_condition.value = float(vp[2].find('.//Dirichlet_boundary_condition').text)
+        if vp[2].find('.//Dirichlet_boundary_condition').attrib['enabled'].lower() == 'true':
+          self.glom_bm_distance_Dirichlet_boundary_condition_toggle.value = True
+        else:
+          self.glom_bm_distance_Dirichlet_boundary_condition_toggle.value = False
+
         if uep.find('.//options//calculate_gradients').text.lower() == 'true':
           self.calculate_gradient.value = True
         else:
@@ -215,6 +311,18 @@ class MicroenvTab(object):
 
         vp[0].find('.//Dirichlet_options//boundary_value[6]').text = str(self.oxygen_Dirichlet_boundary_condition_value_zmax.value)
         vp[0].find('.//Dirichlet_options//boundary_value[6]').attrib['enabled'] = str(self.oxygen_Dirichlet_boundary_condition_toggle_zmax.value).lower()
+
+        vp[1].find('.//diffusion_coefficient').text = str(self.glom_cap_distance_diffusion_coefficient.value)
+        vp[1].find('.//decay_rate').text = str(self.glom_cap_distance_decay_rate.value)
+        vp[1].find('.//initial_condition').text = str(self.glom_cap_distance_initial_condition.value)
+        vp[1].find('.//Dirichlet_boundary_condition').text = str(self.glom_cap_distance_Dirichlet_boundary_condition.value)
+        vp[1].find('.//Dirichlet_boundary_condition').attrib['enabled'] = str(self.glom_cap_distance_Dirichlet_boundary_condition_toggle.value).lower()
+
+        vp[2].find('.//diffusion_coefficient').text = str(self.glom_bm_distance_diffusion_coefficient.value)
+        vp[2].find('.//decay_rate').text = str(self.glom_bm_distance_decay_rate.value)
+        vp[2].find('.//initial_condition').text = str(self.glom_bm_distance_initial_condition.value)
+        vp[2].find('.//Dirichlet_boundary_condition').text = str(self.glom_bm_distance_Dirichlet_boundary_condition.value)
+        vp[2].find('.//Dirichlet_boundary_condition').attrib['enabled'] = str(self.glom_bm_distance_Dirichlet_boundary_condition_toggle.value).lower()
 
 
         uep.find('.//options//calculate_gradients').text = str(self.calculate_gradient.value)
